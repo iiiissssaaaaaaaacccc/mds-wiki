5f53f3872ef6e971a7ad7b03ed710cb1bc79e5fa
iiiissssaaaaaaaacccc <iiiissssaaaaaaaacccc@gmail.com>
2026-03-27 19:24:36 -0500

    hide Lore, Easter Eggs, Sketchpad from explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,9 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+  filterFn: (node) => !["Lore", "Easter Eggs", "Sketchpad"].includes(node.name),
+}),
   ],
   right: [
     Component.Graph(),
@@ -62,7 +64,9 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+  filterFn: (node) => !["Lore", "Easter Eggs", "Sketchpad"].includes(node.name),
+}),
   ],
   right: [],
 }
